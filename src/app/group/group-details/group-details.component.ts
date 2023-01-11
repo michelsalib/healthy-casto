8d0bbed7e89ebbdb06a503c58a6deeb24b13140f
Michel Salib <michel.salib@gmail.com>
2023-01-12 00:59:32 +0100

    More group design

--- src/app/group/group-details/group-details.component.ts
+++ src/app/group/group-details/group-details.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { documentId, where } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map, Observable, of, Subject, switchMap } from 'rxjs';
+import { Group } from 'src/app/models/Group';
+import { User } from 'src/app/models/User';
+import { GroupsService } from 'src/app/services/db/groups.service';
+import { UsersService } from 'src/app/services/db/users.service';
 
 @Component({
   selector: 'app-group-details',
   templateUrl: './group-details.component.html',
   styleUrls: ['./group-details.component.scss']
 })
-export class GroupDetailsComponent {
+export class GroupDetailsComponent implements OnInit {
+  dataset$: Observable<{
+    group: Group;
+    users: User[] | null;
+  }> = new Subject();
+
+  constructor(private route: ActivatedRoute, private userDb: UsersService, private groupService: GroupsService) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const groupId = params.get('groupId');
+
+      if (!groupId) {
+        throw 'Required group id missing';
+      }
+
+      this.dataset$ = this.groupService.get(groupId)
+        .pipe(
+          switchMap(group => 
+            combineLatest([
+              of(group),
+              this.userDb.list(where(documentId(), 'in', group.members.slice(0, 10)))
+            ])
+          ),
+          map(([group, users])=> {
+            return {
+              group, users,
+            };
+          })
+        );
+    });
+  }
 
 }
